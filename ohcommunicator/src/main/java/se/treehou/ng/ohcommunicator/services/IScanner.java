f28400a2d935509748a081172ff4e38abef2df25
ibaton <mattias.markehed@gmail.com>
2017-11-26 12:13:54 +0100

    Update IScanner return type

--- ohcommunicator/src/main/java/se/treehou/ng/ohcommunicator/services/IScanner.java
+++ ohcommunicator/src/main/java/se/treehou/ng/ohcommunicator/services/IScanner.java
@@ -1,7 +1,8 @@
 package se.treehou.ng.ohcommunicator.services;
 
 import io.reactivex.Observable;
+import se.treehou.ng.ohcommunicator.connector.models.OHServer;
 
 public interface IScanner {
-    Observable registerRx();
+    Observable<OHServer> registerRx();
 }
